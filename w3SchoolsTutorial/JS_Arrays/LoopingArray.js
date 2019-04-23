c8eeb8c11408417ab8846db85b390813ec7fdc0a
Claude Christ <christ.tchambila@gmail.com>
2019-04-23 16:39:31 +0200

    JS tutorials tenth day

---
+++ w3SchoolsTutorial/JS_Arrays/LoopingArray.js
@@ -0,0 +1,8 @@
+let array = ["John", "Doe", "Michael", "Carrick", "James", "LeNoir"];
+let names = "";
+
+for (let i = 0; i < array.length; i++) {
+    names += array[i] + "\n";
+}
+
+console.log(names);
